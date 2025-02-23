98fbc426007a464789b2299c3185a9f5d4eb5f8e
vijay-ananth <vijayananth1992000@gmail.com>
2025-02-23 10:58:51 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let userName = "Vijay";
+let age = 25;
+let isExist = true;
+let hobbies = ["Reading", "Coding", "Travelling"];
+let mixed = ["Vijay", 25, "Reading"];
+let mixed2 = ["Vijay", 25, "Reading", true];
+let person = {
+    name: "Vijay",
+    age: 25,
+    isExist: true,
+};
+console.log(userName, age, isExist, mixed, mixed2, person);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let userName: string = "Vijay";
+let age: number = 25;
+let isExist: boolean = true;
+let hobbies: string[] = ["Reading", "Coding", "Travelling"];
+let mixed: (string | number)[] = ["Vijay", 25, "Reading"];
+let mixed2: any[] = ["Vijay", 25, "Reading", true];
+let person: object = {
+  name: "Vijay",
+  age: 25,
+  isExist: true,
+};
+
+
+console.log(userName, age, isExist, mixed, mixed2, person);
